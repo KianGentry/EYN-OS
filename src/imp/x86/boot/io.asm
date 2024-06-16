6120082bc76239d8757a8ed3d7903c7339dcda14
Kian Gentry <101657439+KianGentry@users.noreply.github.com>
2024-06-16 23:36:25 +0100

    Add files via upload

---
+++ src/imp/x86/boot/io.asm
@@ -0,0 +1,50 @@
+; code from Arjun Sreedharan, under the GNU General Public license
+
+bits 64
+section .text
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002              ;magic
+        dd 0x00                    ;flags
+        dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
+
+global start1
+global keyboard_handler
+global read_port
+global write_port
+global load_idt
+
+extern kernel_main 		;this is defined in the c file
+extern keyboard_handler_main
+
+read_port:
+	mov edx, [esp + 4]
+			;al is the lower 8 bits of eax
+	in al, dx	;dx is the lower 16 bits of edx
+	ret
+
+write_port:
+	mov   edx, [esp + 4]    
+	mov   al, [esp + 4 + 4]  
+	out   dx, al  
+	ret
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti 				;turn on interrupts
+	ret
+
+keyboard_handler:                 
+	call    keyboard_handler_main
+	iretd
+
+start1:
+	cli 				;block interrupts
+	mov esp, stack_space
+	call kernel_main
+	hlt 				;halt the CPU
+
+section .bss
+resb 4096*2; 8KB for stack
+stack_space:
