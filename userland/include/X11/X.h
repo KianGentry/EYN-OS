1d6906189a71aa3fd5f0d66b6a7134f9a741810e
Kian Gentry <musinks@pm.me>
2026-03-12 20:05:31 +0000

    X: change structure of X.h a little bit

--- userland/include/X11/X.h
+++ userland/include/X11/X.h
@@ -72,7 +72,7 @@
 #define SelectionClear          29
 #define SelectionRequest        30
 #define SelectionNotify         31
-#define ColourmapNotify          32
+#define ColourmapNotify         32
 #define ClientMessage           33
 #define MappingNotify           34
 #define LASTEvent               35
@@ -82,32 +82,32 @@
  *  Event masks
  * -----------------------------------------------------------------
  */
-#define NoEventMask             0L
-#define KeyPressMask            (1L << 0)
-#define KeyReleaseMask          (1L << 1)
-#define ButtonPressMask         (1L << 2)
-#define ButtonReleaseMask       (1L << 3)
-#define EnterWindowMask         (1L << 4)
-#define LeaveWindowMask         (1L << 5)
-#define PointerMotionMask       (1L << 6)
-#define PointerMotionHintMask   (1L << 7)
-#define Button1MotionMask       (1L << 8)
-#define Button2MotionMask       (1L << 9)
-#define Button3MotionMask       (1L << 10)
-#define Button4MotionMask       (1L << 11)
-#define Button5MotionMask       (1L << 12)
-#define ButtonMotionMask        (1L << 13)
-#define KeymapStateMask         (1L << 14)
-#define ExposureMask            (1L << 15)
-#define VisibilityChangeMask    (1L << 16)
-#define StructureNotifyMask     (1L << 17)
-#define ResizeRedirectMask      (1L << 18)
-#define SubstructureNotifyMask  (1L << 19)
+#define NoEventMask              0L
+#define KeyPressMask             (1L << 0)
+#define KeyReleaseMask           (1L << 1)
+#define ButtonPressMask          (1L << 2)
+#define ButtonReleaseMask        (1L << 3)
+#define EnterWindowMask          (1L << 4)
+#define LeaveWindowMask          (1L << 5)
+#define PointerMotionMask        (1L << 6)
+#define PointerMotionHintMask    (1L << 7)
+#define Button1MotionMask        (1L << 8)
+#define Button2MotionMask        (1L << 9)
+#define Button3MotionMask        (1L << 10)
+#define Button4MotionMask        (1L << 11)
+#define Button5MotionMask        (1L << 12)
+#define ButtonMotionMask         (1L << 13)
+#define KeymapStateMask          (1L << 14)
+#define ExposureMask             (1L << 15)
+#define VisibilityChangeMask     (1L << 16)
+#define StructureNotifyMask      (1L << 17)
+#define ResizeRedirectMask       (1L << 18)
+#define SubstructureNotifyMask   (1L << 19)
 #define SubstructureRedirectMask (1L << 20)
-#define FocusChangeMask         (1L << 21)
-#define PropertyChangeMask      (1L << 22)
+#define FocusChangeMask          (1L << 21)
+#define PropertyChangeMask       (1L << 22)
 #define ColourmapChangeMask      (1L << 23)
-#define OwnerGrabButtonMask     (1L << 24)
+#define OwnerGrabButtonMask      (1L << 24)
 
 /*
  * -----------------------------------------------------------------
